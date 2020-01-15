dc07ef51f6cad9570cca3aec7aa20ff05627c85e
veronicamaliseva <59717393+veronicamaliseva@users.noreply.github.com>
2020-01-15 16:16:36 +0200

    config

---
+++ config/services.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'facebook' => [
+       'client_id' => '2491135317820528',
+       'client_secret' => '69bc2b7a83eb1c1f660e595c1c37d942',
+       'redirect' => 'http://localhost/login/facebook/callback',
+    ],
+
+    
+];
